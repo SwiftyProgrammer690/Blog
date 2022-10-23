7924044169901e5317c9845a121dcc6550b228da
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2022-10-22 21:22:52 -0400

    i dunno man

--- index.ts
+++ index.ts
@@ -4,11 +4,14 @@ blog({
   title: "Swarit's Wall",
   author: "Swarit Choudhari",
   avatar: "./avatar.svg",
-  description: "I mainly post about chess, coding, and my life!",
+  description: "Chess state champion, coder/gamedev, musician, singer, and everythin in between!",
   avatarClass: "full",
   links: [
     { title: "Email", url: "mailto:swaritchoudhari.p.s.y@gmail.com" },
     { title: "GitHub", url: "https://github.com/SwiftyProgrammer690" },
+    { title: "Twitter", url: "https://twitter.com/ChoudhariSwarit"},
+    { title: "Replit", url: "https://replit.com/@SwaritChoudhari"}
+    { title: "Chess.com", url: "https://www.chess.com/member/chess_monk1"}
   ],
   background: "#fff",
   favicon: "favicon.ico"
