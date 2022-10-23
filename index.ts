91a82d4525cc6b646c01a882fa05642e55150794
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2022-10-23 14:58:00 -0400

    Added avatar

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
 blog({
   title: "Swarit's Wall",
   author: "Swarit Choudhari",
-  avatar: "./avatar.svg",
+  avatar: "./Pfpboi-pfp.png",
   description: "Chess state champion, coder/gamedev, musician, singer, and everythin in between!",
-  avatarClass: "full",
+  avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:swaritchoudhari.p.s.y@gmail.com" },
     { title: "GitHub", url: "https://github.com/SwiftyProgrammer690" },
